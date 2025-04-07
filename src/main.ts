6fb7b889573d798179ff71c8090699c4993d49f6
tencalink0 <bencax270@gmail.com>
2025-04-07 17:46:47 +0100

    feat: multi-file test data input

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,27 @@ import { WordMapping } from "./wordmap.js";
 import { Analyser } from "./analyse.js";
 import { promises as fs } from 'fs';
 
-async function loadData() {
-    try {
-        const testData = await fs.readFile('./training_data/test_data.txt', 'utf8');
-        return testData;
-    } catch (err) {
-        // TODO: better fallback
-        console.log("Failed to load, using fallback...");
-        return "A cow likes to eat chicken";
+async function loadData(textFileArr: Array<string>) {
+    let accumulatedData = '';
+
+    for (const file of textFileArr) {
+        try {
+            const fileData = await fs.readFile(file, 'utf8');
+            accumulatedData += fileData; 
+        } catch (err) {
+            //TODO: better fallback
+            console.log(`Failed to load ${file}, using fallback...`);
+            accumulatedData += "A cow likes to eat chicken";
+        }
     }
+
+    return accumulatedData;
 }
 
-loadData().then(testData => {
+loadData([
+    './training_data/test_data2.txt',
+    './training_data/test_data.txt'
+]).then(testData => {
     let analyser = new Analyser();
     analyser.input(testData);
     
